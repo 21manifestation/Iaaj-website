55882d31d70cbe4e1f9e5bfdfe64c2107fefc363
Gaurav Pawar <gauravp1996@gmail.com>
2026-07-18 19:43:54 +0530

    Add Google Analytics 4 via shared analytics.js on all pages (Meta Pixel ready)

---
+++ js/analytics.js
@@ -0,0 +1,29 @@
+// Shared tracking for all IAAJ pages. Loaded in the <head> of every page.
+
+// ---- Google Analytics 4 ----
+(function () {
+  var GA_ID = 'G-ED7CTVGJYY';
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=' + GA_ID;
+  document.head.appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', GA_ID);
+})();
+
+// ---- Meta (Facebook) Pixel ----
+// Paste the Pixel ID between the quotes once available, then it activates automatically.
+(function () {
+  var PIXEL_ID = '';
+  if (!PIXEL_ID) return;
+  !function (f, b, e, v, n, t, s) {
+    if (f.fbq) return; n = f.fbq = function () { n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments); };
+    if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = [];
+    t = b.createElement(e); t.async = !0; t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s);
+  }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', PIXEL_ID);
+  fbq('track', 'PageView');
+})();
